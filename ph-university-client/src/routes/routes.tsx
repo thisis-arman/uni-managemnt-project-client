de73a743cfe5e9f7d3f9813bdd291bccc38505c1
Arman Hossen <armanhossen1549@gmail.com>
2024-03-16 23:24:41 +0600

    route generator reuseable function

--- ph-university-client/src/routes/routes.tsx
+++ ph-university-client/src/routes/routes.tsx
@@ -1,35 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { routeGenerator } from "../utils/routeGenerator";
 import adminPaths from "./admin.routes";
-import { ReactNode } from "react";
 
-
-type TRoute = {
-    path: string,
-    element:ReactNode
-}
-
-const adminRoutes = adminPaths.reduce((acc :TRoute[], item ) => {
-    if (item.path && item.element) {
-        acc.push({
-            path: item.path,
-            element: item.element
-        })
-    }
-    if (item.children) {
-        item.children.forEach((child) => {
-            acc.push(
-                {
-                    path: child.path,
-                    element: child.element
-                }
-            )
-        })
-    }
-    return acc;
-}, [] as TRoute[])
-
-console.log(adminRoutes)
 
 const router = createBrowserRouter([
     {
@@ -39,17 +12,17 @@ const router = createBrowserRouter([
     {
         path: '/admin',
         element: <App />,
-        children: adminRoutes
+        children: routeGenerator(adminPaths)
     },
     {
         path: '/faculty',
         element: <App />,
-        children: adminRoutes
+        children: routeGenerator(adminPaths)
     },
     {
         path: '/student',
         element: <App />,
-        children: adminRoutes
+        children: routeGenerator(adminPaths)
     },
 ])
 
