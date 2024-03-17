751d2af73a60dda9109318ddcbf7e7f2521cd116
Arman Hossen <armanhossen1549@gmail.com>
2024-03-17 14:01:50 +0600

    middeware conntect in store

--- ph-university-client/src/redux/store.tsx
+++ ph-university-client/src/redux/store.tsx
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice'
+import { baseApi } from "./api/baseApi";
 
 
  export const store = configureStore({
      reducer: {
+         [baseApi.reducerPath]: baseApi.reducer,
         auth:authReducer
-    }
+     },
+     middleware: (getDefaultMiddleware) => {
+         getDefaultMiddleware().concat(baseApi.middleware)
+     }
 })
 
 
