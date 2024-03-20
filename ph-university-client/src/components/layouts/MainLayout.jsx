7f8fed2cee85a02236d3593bcbf52c618acfcb35
Arman Hossen <armanhossen1549@gmail.com>
2024-03-20 06:18:08 +0600

    logout button

--- ph-university-client/src/components/layouts/MainLayout.jsx
+++ ph-university-client/src/components/layouts/MainLayout.jsx
@@ -1,17 +1,28 @@
-import {  Layout,  } from 'antd';
+import {  Button, Layout,  } from 'antd';
 import React from 'react';
 import {  Outlet } from 'react-router-dom';
 const { Header, Content, Footer } = Layout;
 import Sidebar from './Sidebar';
+import {useAppDispatch} from '../../redux/hooks'
+import { logout } from '../../redux/features/auth/authSlice';
 
 
 const MainLayout = () => {
+
+    const dispatch = useAppDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+        
+    }
     
     return (
         <Layout style={{height:"100vh"}}>
             <Sidebar/>
             <Layout>
-                <Header style={{ padding: 0 }} />
+                <Header style={{ padding: 0 }} onClick={handleLogout} >
+                    <Button>logout</Button>
+                </Header>
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div
                         style={{
